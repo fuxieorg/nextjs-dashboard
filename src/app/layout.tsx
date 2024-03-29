85db931b40de03e30f4ae752572560d21890489b
Fu Xie <fuxieorg@gmail.com>
2024-03-30 03:30:20 +0800

    chore: remove richColors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <Toaster richColors />
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
