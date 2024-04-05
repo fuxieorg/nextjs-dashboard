bf4e8c06e547310c403cf3d322da91ee2748ce7b
Fu Xie <fuxieorg@gmail.com>
2024-04-05 17:19:18 +0800

    feat: add MainLayout

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import Sidebar from "@/components/sidebar";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: Props) {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Sidebar />
+      <div className="flex flex-col">
+        <Header />
+        <div className="container h-full">
+          <main className="grid h-full flex-1 grid-rows-[auto,1fr] gap-4 p-4 md:p-8 lg:gap-6">
+            {children}
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
